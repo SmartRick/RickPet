7927764582af47295992594a39f4d89d826c297f
MBZ986 <2869748651@qq.com>
2021-07-20 17:36:28 +0800

    2021年7月20日

--- modules/util1.py
+++ modules/util1.py
@@ -1,7 +1,9 @@
 import os
 
 import win32api, win32gui, win32con
+from win32com.client import Dispatch
 import time
+
 
 # 更换windows桌面壁纸
 def setWallPaper(pic):
@@ -14,20 +16,34 @@ def setWallPaper(pic):
     # 刷新桌面
     win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, pic, win32con.SPIF_SENDWININICHANGE)
 
-def openexe():
-    # system = os.system("‪E:\\谷歌下载\\鲸猫试用小助手1.3.14.exe")
+
+def openexe(path):
+    # with open(path, 'r') as file:
+    #     print(file.name)
+    execute = win32api.ShellExecute(0, 'open', path, '', '', 1)  # 前台打开
+    # win32api.ShellExecute(0, 'open', path, '', '', 1)  # 前台打开
+    # win32api.ShellExecute(0, 'open', path, '1.txt', '', 1)  # 打开文件
+    # win32api.ShellExecute(0, 'open', 'http://www.sohu.com', '', '', 1)  # 打开网页
+    # win32api.ShellExecute(0, 'open', 'D:\\Opera.mp3', '', '', 1)  # 播放视频
+    # win32api.ShellExecute(0, 'open', 'D:\\hello.py', '', '', 1)  # 运行程序
+    # print(execute)
+    # system = os.system(path)
+    # print(system)
     # 执行程序
     # popen = os.popen(r"python D:\pythonProbject\PyQt5Demo\hello.py")
-    pythonStr = r"python D:\pythonProbject\PyQt5Demo\hello.py"
     # 执行cmd命令
     # popen = os.popen("dir")
+    # print(popen.read())
 
-    popen = os.popen("dir")
-    # print(system)
-    print(popen.read())
-    # win32api.ShellExecute(0, 'open', '‪E:\\谷歌下载\\鱼牙直播姬V1.24\\鱼牙直播姬V1.24.exe', '', '', 1)           # 前台打开
 
-if __name__=='__main__':
+shell = Dispatch("WScript.Shell")
+# 获取快捷方式的真实路径
+def getLnkPath(path):
+    return shell.CreateShortCut(path).Targetpath
+
+if __name__ == '__main__':
     # pic='''D:\\pythonProbject\\PyQt5Demo\\images\\bz.jpg'''
     # setWallPaper(pic)
-    openexe()
+    # openexe('E:\\softword\\CloudMusic\\cloudmusic.exe')
+    # openexe('../鲸猫试用小助手.lnk')
+    getLnkPath('../鲸猫试用小助手.lnk')
